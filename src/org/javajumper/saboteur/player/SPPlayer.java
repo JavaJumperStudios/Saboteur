89a6e21fe601123e434caecefa73ed2ac98e8b25
Jakob Steeg <jakob.steeg@unitybox.de>
2015-08-06 15:03:16 +0200

    Clientseitige Verarbeitung von Snapshots hinzugefügt. Geschwindigkeit des Spielers verringert. Fehler beseitigt. Fixes #2

--- src/org/javajumper/saboteur/player/SPPlayer.java
+++ src/org/javajumper/saboteur/player/SPPlayer.java
@@ -8,7 +8,7 @@ public class SPPlayer extends Player {
 
     private Image image;
     private String texture;
-    
+
     public SPPlayer(int id, Role role, String name, int livepoints,
 	    Vector2f pos, String texture) {
 	super(id, role, name, livepoints, pos);
@@ -18,15 +18,15 @@ public class SPPlayer extends Player {
 
     @Override
     public void update(int delta) {
-	
+
     }
-    
+
     public void draw(float x, float y) {
 	if (image == null)
 	    image = RessourceManager.loadImage(texture);
-	
+
 	image.draw(x, y);
-	
+
 	if (this.getInventory()[this.getCurrentWeapon()] != null)
 	    this.getInventory()[this.getCurrentWeapon()].draw(x, y);
 
