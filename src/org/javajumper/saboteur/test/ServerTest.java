d9570d03620ed61b5831bbe0c7891186d8acc310
Jakob Steeg <jakob.steeg@gmail.com>
2016-06-22 15:43:34 +0200

    Resolved conflicts

--- src/org/javajumper/saboteur/test/ServerTest.java
+++ src/org/javajumper/saboteur/test/ServerTest.java
@@ -1,4 +1,5 @@
 package org.javajumper.saboteur.test;
+
 import java.util.logging.Logger;
 
 import org.javajumper.saboteur.SaboteurServer;
@@ -8,7 +9,7 @@ public class ServerTest {
 
 	public static void main(String[] _args) throws SlickException, InterruptedException {
 		Logger logger = Logger.getLogger(SaboteurServer.class.getName());
-		
+
 		SaboteurServer server = new SaboteurServer();
 		SaboteurServer.instance = server;
 		logger.info("Server wird gestartet.");
