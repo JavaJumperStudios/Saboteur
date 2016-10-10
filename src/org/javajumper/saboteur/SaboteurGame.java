bec154cc700d00cae7f2dbc63ef1812e8f4f5b30
Jakob Steeg <jakob.steeg@unitybox.de>
2016-10-10 12:33:57 +0200

    Fixes #75
    Tranzparenz-Bug gefixt

--- src/org/javajumper/saboteur/SaboteurGame.java
+++ src/org/javajumper/saboteur/SaboteurGame.java
@@ -53,6 +53,7 @@ public class SaboteurGame extends BasicGameState {
 	private Map map;
 	private SPPlayer thePlayer;
 	private Image gui;
+	private Image mapImage;
 
 	private ArrayList<SPPlayer> players = new ArrayList<>();
 	private ArrayList<DeadPlayer> deadplayers = new ArrayList<>();
@@ -83,6 +84,8 @@ public class SaboteurGame extends BasicGameState {
 
 		background = RessourceManager.loadImage("background.png");
 		Tile.initTileRendering();
+
+		mapImage = new Image(background.getWidth(), background.getHeight());
 	}
 
 	/**
@@ -187,7 +190,7 @@ public class SaboteurGame extends BasicGameState {
 				serverListener.sendToServer(packet10);
 				System.out.println("I changed ready state to: " + ready);
 			}
-			
+
 			if (input.isKeyPressed(Input.KEY_ESCAPE)) {
 				closeConnection();
 				StateManager.changeState(0, new FadeOutTransition(Color.black, 1000),
@@ -213,15 +216,7 @@ public class SaboteurGame extends BasicGameState {
 		}
 	}
 
-	private void renderInGameScreen(Graphics g) {
-		g.clearAlphaMap();
-		g.setDrawMode(Graphics.MODE_ALPHA_MAP);
-		g.setColor(Color.black);
-
-		renderShadows(g);
-
-		g.setDrawMode(Graphics.MODE_ALPHA_BLEND);
-
+	private void renderMap(Graphics g) {
 		map.draw(g);
 
 		for (SPPlayer p : players) {
@@ -232,6 +227,24 @@ public class SaboteurGame extends BasicGameState {
 		for (DeadPlayer p : deadplayers) {
 			p.draw();
 		}
+	}
+
+	private void renderInGameScreen(Graphics g) throws SlickException {
+		g.clearAlphaMap();
+		g.setDrawMode(Graphics.MODE_ALPHA_MAP);
+		g.setColor(Color.black);
+
+		renderShadows(g);
+
+		g.setDrawMode(Graphics.MODE_ALPHA_BLEND);
+
+		Graphics mapImgG = mapImage.getGraphics();
+
+		// Rendering the map to an image to prevent mixing up transparency of
+		// objects and the shadow
+		renderMap(mapImgG);
+
+		g.drawImage(mapImage, 0, 0);
 
 		// TODO draw the gui in Graphics.MODE_NORMAL after
 		// gui.draw();
@@ -351,8 +364,8 @@ public class SaboteurGame extends BasicGameState {
 	 *            the endcause to set
 	 *            <ul>
 	 *            <li>{@link SaboteurServer#TIME_RAN_OUT} = Zeit abgelaufen</li>
-	 *            <li>{@link SaboteurServer#NO_TRAITORS_LEFT} = Alle Traitor
-	 *            tot</li>
+	 *            <li>{@link SaboteurServer#NO_TRAITORS_LEFT} = Alle Traitor tot
+	 *            </li>
 	 *            <li>{@link SaboteurServer#NO_INNOCENTS_LEFT} = Alle Innocents
 	 *            tot</li>
 	 *            <li>{@link SaboteurServer#NO_PLAYERS_LEFT} = Alle tot</li>
@@ -487,8 +500,8 @@ public class SaboteurGame extends BasicGameState {
 	 * 
 	 * @param origin
 	 *            the origin of the line whose arc shall be calculated
-	 * @param point 
-	 * 	          the point to which the angle shall be calculated
+	 * @param point
+	 *            the point to which the angle shall be calculated
 	 * @return the angle from the center of the player to the given point
 	 */
 	public double getAngle(Vector2f origin, Vector2f point) {
@@ -674,9 +687,10 @@ public class SaboteurGame extends BasicGameState {
 		serverListener.sendToServer(packet);
 		serverListener.close();
 	}
-	
+
 	/**
 	 * Deletes all underscores ('_') from a given text
+	 * 
 	 * @param text
 	 * @return the given text without underscores
 	 */
